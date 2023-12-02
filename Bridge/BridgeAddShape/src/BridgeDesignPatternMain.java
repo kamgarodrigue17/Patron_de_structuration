398ebcff45027422f5c2acabef385f7530b357eb
kamgarodrigue17 <chocobami5@gmail.com>
2023-12-02 19:54:41 +0100

    patron bridge fin de BridgeAddShape

--- Bridge/BridgeAddShape/src/BridgeDesignPatternMain.java
+++ Bridge/BridgeAddShape/src/BridgeDesignPatternMain.java
@@ -1,5 +1,6 @@
 public class BridgeDesignPatternMain {
     public static void main(String[] args) throws Exception {
+        // tet
         Shape s1=new Triangle(new RedColor());
         s1.colorIt();
         Shape s2=new Circle(new BlueColor());
